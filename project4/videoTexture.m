e99afed1250b927294aceae6061c9707cf15a9d7
higerra <higerra@126.com>
2016-04-04 23:03:25 -0500

    Hang

--- project4/videoTexture.m
+++ project4/videoTexture.m
@@ -4,15 +4,20 @@ vreader = VideoReader(filename);
 w = vreader.Width;
 h = vreader.Height;
 fps = vreader.FrameRate;
-
-maxKFrame = 1000;
+fprintf('Width: %d, height: %d, fps: %d\n', w, h, fps);
+maxKFrame = 2000;
 kFrame = 0;
 tempframes = cell(maxKFrame);
+interval = 2;
+icount = 0;
 for i=1:maxKFrame
     if hasFrame(vreader)
-        fprintf('Reading frame %d\n', i);
-        tempframes{i} = im2double(readFrame(vreader));
-        kFrame = kFrame+1;
+        if mod(icount,interval) == 0
+            tempframes{kFrame+1} = im2double(readFrame(vreader));
+            kFrame = kFrame+1;
+            icount = 0;
+        end
+        icount = icount+1;
     end
 end
 kFrame = kFrame - 1;
@@ -27,12 +32,12 @@ disp('Computing initial D matrix...');
 D = zeros(kFrame, kFrame);
 for i=1:kFrame
     for j=1:kFrame
-        I1 = reshape(frames{i}, 1, []);
-        I2 = reshape(frames{j}, 1, []);
-        D(i,j) = sqrt((I1-I2) * (I1-I2)');
+        D(i,j) = sqrt(sum(sum(sum((frames{i} - frames{j}).^2))));
+%         I1 = reshape(frames{i}, 1, []);
+%         I2 = reshape(frames{j}, 1, []);
+%         D(i,j) = sqrt((I1-I2) * (I1-I2)');
     end
 end
-%D(kFrame+1,:) = max(max(D));
 disp('Done');
 
 %diagnal filter
@@ -69,7 +74,7 @@ while true
         Dq(:,j) = D(:,j).^constp + alpha*mj(j+1);
     end
     diff = sqrt(sum(sum((Dq - Dq2).^2)));
-    fprintf('iteration %d, error %.3f\n', iterCount, diff);
+    %fprintf('iteration %d, error %.3f\n', iterCount, diff);
     if diff <= converge_th
         break;
     end
@@ -79,38 +84,73 @@ D = Dq;
 
 %compute P matrix
 aveD = mean(mean(D(m+1:kFrame-m+1, m+1:kFrame-m+1)));
-%sigma = 0.1 * aveD;
-sigma = 2;
+
+sigma = 0.02 * aveD;
+fprintf('aveD:%.3f, sigma:%.3f\n', aveD, sigma);
+%sigma = 5;
 P = zeros(kFrame, kFrame);
-for i=m+1:kFrame-m+1
-    for j=m+1:kFrame-m+1
+for i=m+1:kFrame-m
+    for j=m+1:kFrame-m
         P(i,j) = exp(-1*D(i+1,j)/sigma);
     end
    P(i,:) = P(i,:) / sum(P(i,:));
 end
-
-Dv = D./max(max(D));
-figure(1),
-hold on;
-subplot(1,2,1);
-imshow(Dv);
-title('D matrix');
-subplot(1,2,2);
-Pv = P./max(max(P));
-imshow(Pv);
-title('P Matrix');
-hold off;
+%pruning
+%local maxima
+P2 = P;
+for i=m+1:kFrame-m+1
+    for j=m+2:kFrame-m
+        if P(i,j) < P(i,j-1) || P(i,j) < P(i,j+1)
+            P2(i,j) = 0;
+        end
+    end
+end
+P = P2;
+%thresholding
+P(P<0.05) = 0.0;
+%double probability of forward jump, set probability of backward jump to 0
+amp = 2;
+for i=m+1:kFrame-m+1
+    for j=i+2:kFrame-m+1
+        P(i,j) = P(i,j) * amp;
+    end
+    for j=m+1:i
+        P(i,j) = 0.0;
+    end
+end
+%re-normalization
+for i=m+1:kFrame-m+1
+    P(i,:) = P(i,:) / sum(P(i,m+1:kFrame-m));
+end
+% Dv = D./max(max(D(m+1:kFrame-m+1, m+1:kFrame-m+1)));
+ figure(1),
+% hold on;
+% subplot(1,2,1);
+% imshow(Dv);
+% title('D matrix');
+% subplot(1,2,2);
+ Pv = P./max(max(P));
+ imshow(Pv);
+ title('P Matrix');
+% hold off;
 
 %cdf from pdf
 cdf = cumsum(P,2);
-source = kFrame-1;
-kOutput = 1000;
+source = m+1;
+kOutput = max(kFrame * 2, 1000);
 
-figure(2),
-hold on;
+vwriter = VideoWriter(strcat('result_',filename), 'MPEG-4');
+vwriter.FrameRate = fps;
+open(vwriter);
+
+% figure(2),
+% hold on;
 x = rand(kOutput,1);
 for i=1:kOutput
     %draw from distribution
+    if source >= kFrame - 10
+        break;
+    end
     target = m+1;
     if x > cdf(source,1)
         for j=m+1:kFrame - m
@@ -120,10 +160,12 @@ for i=1:kOutput
             end
         end
     end
-    fprintf('Jump from frame %d to %d, p: %.3f\n', source, target, P(source,target));
-    imshow(frames{target});
+    fprintf('Writing frame %d, Jump from frame %d to %d, p: %.3f\n', i, source, target, P(source,target));
+    %imshow(frames{target});
+    writeVideo(vwriter, frames{target});
     source = target;
-    pause(1.0/(fps+2));
+    %pause(1.0/(fps+2));
 end
-hold off;
+% hold off;
+close(vwriter);
 end
